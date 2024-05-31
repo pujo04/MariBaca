4f9e03de13eb6eda265b14dff9792cc96ad2bec5
Andreas <93956486+pujo04@users.noreply.github.com>
2024-05-31 10:54:16 +0700

    Add files via upload

--- MariBaca/peminjaman.php
+++ MariBaca/peminjaman.php
@@ -1,3 +1,54 @@
 <?php
   include "header.php";
+  $query=mysqli_query($db, "SELECT * FROM data_buku");
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12 mt-3" style="min-height: 462px";>
+      <div class="card">
+        <div class="card-header">
+          Daftar Peminjaman
+        </div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <a href="data_buku.php" class="btn btn-primary">Tambah Peminjaman</a>
+            </div>
+          </div>
+          <div class="row mt-3">
+            <div class="col">
+              <table class="table table-bordered tables-striped">
+                <tr>
+                  <th>No</th>
+                  <th>ID Peminjaman</th>
+                  <th>ID Anggota</th>
+                  <th>ID Buku</th>
+                  <th>Tanggal Peminjaman</th>
+                  <th>Status Peminjamman</th>
+                  </tr>
+                  <?php
+                    $no=1;
+                    while ($ambil_data=mysqli_fetch_array($query)) {
+                  ?>
+                <td><?php echo $no++ ?></td>
+                <td><?php echo $ambil_data['id_peminjaman']; ?></td>
+                <td><?php echo $ambil_data['id_anggota']; ?></td>
+                <td><?php echo $ambil_data['id_buku']; ?></td>
+                <td><?php echo $ambil_data['tgl_peminjaman']; ?></td>
+                <td><?php echo $ambil_data['status_pinjam']; ?></td>
+               <?php
+                  }
+               ?>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+  Include "footer.html";
 ?>
--- MariBaca/pengembalian.php
+++ MariBaca/pengembalian.php
@@ -1,3 +1,54 @@
 <?php
   include "header.php";
+  $query=mysqli_query($db, "SELECT * FROM data_buku");
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12 mt-3" style="min-height: 462px";>
+      <div class="card">
+        <div class="card-header">
+          Daftar Pengembalian
+        </div>
+        <div class="card-body">
+          <div class="row">
+             <div class="col-sm">
+                  Daftar berikut merupakan Buku yang belum kembali, tekan kotak merah pada tabel aksi
+             </div>
+          </div>
+          <div class="row mt-3">
+            <div class="col">
+              <table class="table table-bordered tables-striped">
+                <tr>
+                  <th>No</th>
+                  <th>ID Pengembalian</th>
+                  <th>ID Peminjaman</th>
+                  <th>Tanggal Pengembalian</th>
+                  <th>Aksi</th>
+                  </tr>
+                  <?php
+                    $no=1;
+                    while ($ambil_data=mysqli_fetch_array($query)) {
+                  ?>
+                <td><?php echo $no++ ?></td>
+                <td><?php echo $ambil_data['id_pengembalian']; ?></td>
+                <td><?php echo $ambil_data['id_peminjaman']; ?></td>
+                <td><?php echo $ambil_data['tgl_kembali']; ?></td>
+                <td>
+                  <a href="data_buku.php?id=<?php echo $ambil_data['id_buku'] ?>" class="btn btn-danger">Kembalikan</a>
+                </td>
+               <?php
+                  }
+               ?>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+  Include "footer.html";
 ?>
